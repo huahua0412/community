fcfaf42131e8349d3820c8f0682871e59bbe50f7
hua <2373426635@qq.com>
2023-08-08 11:30:39 +0800

    修复注入登录

--- src/main/java/com/hua/community/controller/AuthorizeController.java
+++ src/main/java/com/hua/community/controller/AuthorizeController.java
@@ -14,14 +14,14 @@ public class AuthorizeController
 {
     @Autowired
     private GithubProvider githubProvider;
-    /*
+
     @Value("${github.client.id}")
     private String c_id;
     @Value("${github.client.secret}")
     private String c_sc;
     @Value("${github.redirect.uri}")
     private String ur;
-    */
+
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
@@ -29,16 +29,16 @@ public class AuthorizeController
     {
         AccesstokenDTO accesstokenDTO=new AccesstokenDTO();
 
-        /*
+
         System.out.println(c_id);
         System.out.println(c_sc);
         System.out.println(ur);
-         */
 
-        accesstokenDTO.setClient_id("f32965c43d6030acc89c");
+
+        accesstokenDTO.setClient_id(c_id);
         accesstokenDTO.setCode(code);
-        accesstokenDTO.setClient_secret("71a8afc70b559781ec80f3b837a2404746974eed");
-        accesstokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accesstokenDTO.setClient_secret(c_sc);
+        accesstokenDTO.setRedirect_uri(ur);
         accesstokenDTO.setState(state);
         String accesstoken=githubProvider.getAccessTokenDTO(accesstokenDTO);
         System.out.println(accesstoken);
